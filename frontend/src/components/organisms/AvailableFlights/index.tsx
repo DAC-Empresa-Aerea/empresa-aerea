80939f41436141c0ad6047d5e1e5bfe687b8945f
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-03-23 13:49:24 -0300

    add: AvailableFlights Component

---
+++ frontend/src/components/organisms/AvailableFlights/index.tsx
@@ -0,0 +1,31 @@
+import { Flight } from "../../atoms/FlightBasicInfo";
+import FlightList from "../../molecules/FlightList";
+
+const availableFlightsDataExample: Array<Flight> = [
+  { number: 101, departure: "10:00", arrival: "12:00" },
+  { number: 102, departure: "14:00", arrival: "16:00" },
+  { number: 103, departure: "18:00", arrival: "20:00" },
+];
+
+// Não deixe como opcional, apenas leve a constante acima para o componente pai
+interface AvailableFlightsProps {
+  flights: Array<Flight>;
+}
+
+function AvailableFlights({
+  flights = availableFlightsDataExample,
+}: AvailableFlightsProps) {
+  return (
+    <FlightList
+      title="Voos Disponíveis"
+      flights={flights}
+      buttonText="Selecionar Voo"
+      onFlightClick={(flight) =>
+        alert(`Não implementado | Selecionar Voo ${flight.number}`)
+      }
+      onViewMoreClick={() => alert("Não implementado")}
+    />
+  );
+}
+
+export default AvailableFlights;
