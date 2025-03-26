e3837d88a53e4684140a80789b34bade4f450b88
voidalzk <voidalzk@gmail.com>
2025-03-26 02:35:39 -0300

    feat: add Cart layout and Page and integrate into LoginRoutes

--- frontend/src/routes/LoginRoutes.tsx
+++ frontend/src/routes/LoginRoutes.tsx
@@ -1,18 +1,21 @@
 import { lazy } from "react";
 import Loadable from "../components/organisms/Loadable";
 import CustomerHomePage from "../pages/CustomerHomePage";
+import Cart from "../pages/Cart";
 
 const LoginLayout = Loadable(lazy(() => import("../layouts/Login")));
 const Login = Loadable(lazy(() => import("../pages/Login")));
 const SelfRegistrationLayout = Loadable(lazy(() => import("../layouts/Self-registration")));
 const SelfRegistration = Loadable(lazy(() => import("../pages/SelfRegistration")));
+const CartLayout = Loadable(lazy(() => import("../layouts/Cart")));
+
 
 const LoginRoutes = {
   path: "/",
   children: [
     {
       path: "/",
-      //element: [<LoginLayout />, <SelfRegistrationLayout />],
+      //element: [<LoginLayout />, <SelfRegistrationLayout />, <CartLayout />],
       children: [
         {
           path: "login",
@@ -29,6 +32,16 @@ const LoginRoutes = {
         {
           path: "customer-home",
           element: <h1 className="text-center"><CustomerHomePage /></h1>
+        },
+        {
+          path: "cart",
+          element: <CartLayout />,
+          children: [
+            {
+              index: true,
+              element: <Cart />
+            }
+          ]
         }
       ]
     }
