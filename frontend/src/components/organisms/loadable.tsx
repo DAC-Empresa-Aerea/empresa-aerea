62a4b5d31265ae7faa57f3bfd1ccc8c7077f4f3c
voidalzk <voidalzk@gmail.com>
2025-03-28 01:52:55 -0300

    feat: add 'Comprar Milhas' button to Header and enhance loading indicator in Loadable component

--- frontend/src/components/organisms/loadable.tsx
+++ frontend/src/components/organisms/loadable.tsx
@@ -1,11 +1,21 @@
 import { Suspense } from "react";
 
+
+
 const Loadable = (Component: any) => {
     return (props: any) => (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div className="flex justify-center items-center h-screen ">Loading...</div>}>
             <Component {...props} />
         </Suspense>
     );
 };
 
 export default Loadable;
+
+<div className="flex justify-center items-center min-h-[200px]">
+<div className="flex space-x-2">
+  <div className="w-3 h-3 bg-blue-600 rounded-full animate-bounce"></div>
+  <div className="w-3 h-3 bg-blue-600 rounded-full animate-bounce delay-150"></div>
+  <div className="w-3 h-3 bg-blue-600 rounded-full animate-bounce delay-300"></div>
+</div>
+</div>
