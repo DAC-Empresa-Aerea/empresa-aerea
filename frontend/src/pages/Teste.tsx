60fc67e0c08e3cb39a2462410361acec1dbd1306
voidalzk <voidalzk@gmail.com>
2025-04-01 18:59:31 -0300

    feat: replace Teste page with LandingPage component and update routing

--- frontend/src/pages/Teste.tsx
+++
@@ -1,9 +0,0 @@
-function Teste() {
-  return (
-    <div className="flex items-center justify-center m-auto w-full h-full">
-      Aqui vai ser a landing page
-    </div>
-  );
-}
-
-export default Teste;
