98a3d38cc08dd60cc0a050c5807cb77620be1720
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-04-04 17:15:44 -0300

    feat: commit inicial do MS Employee

---
+++ backend/employee/src/main/java/com/ms/employee/controllers/GenericController.java
@@ -0,0 +1,15 @@
+package com.ms.employee.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class GenericController {
+    
+    @GetMapping("/hello")
+    public String getHello() {
+        return new String("Hello Employee");
+    }
+    
+}
