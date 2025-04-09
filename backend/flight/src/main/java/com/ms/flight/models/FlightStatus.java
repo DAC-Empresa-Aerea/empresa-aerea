9f9fc4793cefa0d403ee2e5c7e50ebb553f2ee66
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-04-08 22:01:02 -0300

    fix: mudanças para ficar mais semelhante a modelagem esperada pelo professor

--- backend/flight/src/main/java/com/ms/flight/models/FlightStatus.java
+++ backend/flight/src/main/java/com/ms/flight/models/FlightStatus.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -23,12 +21,11 @@ public class FlightStatus implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+    @Column(nullable = false, unique = true)
+    private String codigo;
 
     @Column(nullable = false, unique = true)
     private String sigla;
 
-    @Column(nullable = false, unique = true)
     private String descricao;
 }
