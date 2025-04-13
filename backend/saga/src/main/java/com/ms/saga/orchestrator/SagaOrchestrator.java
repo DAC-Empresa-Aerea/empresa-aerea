70b50651e54dbce4982150f44d269fb4a75df318
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-04-13 19:43:05 -0300

    feat: saga create customer funcionando

--- backend/saga/src/main/java/com/ms/saga/orchestrator/SagaOrchestrator.java
+++ backend/saga/src/main/java/com/ms/saga/orchestrator/SagaOrchestrator.java
@@ -2,10 +2,9 @@ package com.ms.saga.orchestrator;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ms.saga.dto.Roles;
 import com.ms.saga.dto.auth.create.CreateAuthRequestDTO;
 import com.ms.saga.dto.auth.create.CreateAuthResponseDTO;
@@ -19,35 +18,16 @@ public class SagaOrchestrator {
     private RabbitTemplate rabbitTemplate;
     
     public CustomerResponseDTO processCreateCustomer(CustomerRequestDTO customer) {
-        Object response = rabbitTemplate.convertSendAndReceive("create.customer.exchange", "create.customer.routing.key", customer);
-        return (CustomerResponseDTO) response;
-
-
-       // Object customerResponseObj = rabbitTemplate.convertSendAndReceive("create.customer.exchange", "create.customer.routing.key", customer);
-
-        // CreateAuthRequestDTO authRequest = new CreateAuthRequestDTO(customer.getEmail(), Roles.CUSTOMER);
-        // Object authResponseObj = 
-        //     rabbitTemplate.convertSendAndReceive("create.auth.exchange", "create.auth.routing.key", authRequest);
-
-        // if (!(customerResponseObj instanceof CustomerResponseDTO)) {
-        //     System.out.println("Erro: resposta de criação do cliente inválida.");
-        //     return null;
-        // }
+        CustomerResponseDTO response = rabbitTemplate.convertSendAndReceiveAsType("create.customer.exchange", "create.customer.routing.key", customer, new ParameterizedTypeReference<CustomerResponseDTO>() {});
         
-        // if (!(authResponseObj instanceof CreateAuthResponseDTO)) {
-        //     System.out.println("Erro: resposta de criação de auth inválida.");
-        //     return null;
-        // }
-        
-        // CustomerResponseDTO createdCustomer = (CustomerResponseDTO) customerResponseObj;
-        // CreateAuthResponseDTO createdAuth = (CreateAuthResponseDTO) authResponseObj;
-        
-        // if (!createdAuth.isSuccess()) {
-        //     System.out.println("Erro: criação de auth falhou.");
-        //     return null;
-        // }
-        
-        // return createdCustomer;
+        CreateAuthResponseDTO authResponse = rabbitTemplate.convertSendAndReceiveAsType("create.auth.exchange", "create.auth.routing.key", new CreateAuthRequestDTO(customer.getEmail(), Roles.CUSTOMER), new ParameterizedTypeReference<CreateAuthResponseDTO>() {});
+
+        if (response == null || !authResponse.isSuccess()) {
+            System.out.println("Erro: resposta de criação do cliente ou de autenticação inválida.");
+            return null;
+        }
+
+        return response;
     }
 
 }
