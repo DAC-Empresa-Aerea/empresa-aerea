e41f1c6d2b560bd4c679e501fe51bbd168338efe
Luccachl <lucca.hmcps@gmail.com>
2025-04-17 08:39:23 -0300

    feat: integra lista de voos com JSON Server

--- frontend/src/pages/customer/SearchFlights.tsx
+++ frontend/src/pages/customer/SearchFlights.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 import SearchAvailables from "../../components/organisms/SearchAvailables";
-import { flightsDataExample } from "../../data/FlightsExample";
 
 function SearchFlights() {
-  return <SearchAvailables flightsList={flightsDataExample} />;
-}
+  // return <SearchAvailables flightsList={flightsDataExample} />;
+  const [flightsList, setFlightsList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+useEffect(() => {
+  fetch("http://localhost:3001/flights")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Erro ao buscar voos disponíveis");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    setFlightsList(data);
+    setLoading(false);
+  })
+  .catch((error) => {
+    console.error("Erro ao buscar dados no JSON Server:", error);
+    setLoading(false);
+  });
+}, []);
+
+if (loading) return <p>Carregando voos...</p>;
+
+  return <SearchAvailables flightsList={flightsList} />;
+}
 export default SearchFlights;
