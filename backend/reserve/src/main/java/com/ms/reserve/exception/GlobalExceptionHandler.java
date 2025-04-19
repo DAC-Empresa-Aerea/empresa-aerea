a70cc41c7bfb6b59a5b44ebb351294576f7a187e
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-04-18 22:56:49 -0300

    feat(ms reserve): SagaResponse + ErrorDTO + Exception

---
+++ backend/reserve/src/main/java/com/ms/reserve/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.ms.reserve.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ms.reserve.dto.error.ErrorDTO;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
