5d626df67c902b8b039791303ee831607a7e257c
Gabriel Speranceta <gabriel.hesperanceta@gmail.com>
2025-04-22 18:51:36 -0300

    refactor: format code for consistency and improve readability in auth and composition services

--- backend/api_gateway/services/proxyService.js
+++ backend/api_gateway/services/proxyService.js
@@ -3,6 +3,7 @@ const proxy = require('express-http-proxy');
 module.exports = (host, basePath) => {
   return proxy(host, {
     proxyReqPathResolver: req => {
+      console.log(`Proxying request to ${host}${basePath}`);
       const suffix = req.url === '/' ? '' : req.url;
       const upstreamPath = basePath + suffix;
       return upstreamPath;
