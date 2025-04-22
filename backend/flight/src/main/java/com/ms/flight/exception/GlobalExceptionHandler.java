6aa39f0a180376cb820990cdc5aa54141d95221d
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-04-22 17:48:20 -0300

    feat: erros e saga response de flight

---
+++ backend/flight/src/main/java/com/ms/flight/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.ms.flight.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ms.flight.dto.error.ErrorDTO;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
+
