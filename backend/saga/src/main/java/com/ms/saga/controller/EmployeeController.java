41fcdb05d1a674385a479843d4ad018054505e39
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-04-21 23:10:36 -0300

    fix: pequeno ajuste de component para controller

--- backend/saga/src/main/java/com/ms/saga/controller/EmployeeController.java
+++ backend/saga/src/main/java/com/ms/saga/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.ms.saga.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ms.saga.orchestrator.EmployeeOrchestrator;
 
-@Component
+@Controller
+@RequestMapping("/funcionarios")
 public class EmployeeController {
     
     @Autowired
