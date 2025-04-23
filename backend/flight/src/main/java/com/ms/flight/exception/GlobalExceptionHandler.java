43731b2ebe08985526abdb9da358052d47af09c7
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-04-22 23:06:43 -0300

    feat: exception para airport

--- backend/flight/src/main/java/com/ms/flight/exception/GlobalExceptionHandler.java
+++ backend/flight/src/main/java/com/ms/flight/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.ms.flight.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,5 +13,17 @@ import com.ms.flight.dto.error.ErrorDTO;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     
+    @ExceptionHandler(AirportNotRegisteredException.class)
+    public ResponseEntity<ErrorDTO> handleAirportNotRegistered(AirportNotRegisteredException ex) {
+        ErrorDTO error = new ErrorDTO(ex.getCode(), ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("erro", ex.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
 }
 
