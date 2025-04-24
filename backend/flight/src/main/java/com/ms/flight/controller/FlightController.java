105382b6a65885fdc5eae075e675c49004015fd0
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-04-24 13:38:21 -0300

    fix: duplicação de rotas get em flight

--- backend/flight/src/main/java/com/ms/flight/controller/FlightController.java
+++ backend/flight/src/main/java/com/ms/flight/controller/FlightController.java
@@ -4,18 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ms.flight.dto.flight.FlightByAirportDTO;
 import com.ms.flight.dto.flight.FlightResponseDTO;
 import com.ms.flight.dto.flight.FlightWithAirportResponseDTO;
 import com.ms.flight.dto.flight.register.RegisterFlightRequestDTO;
-import com.ms.flight.model.Flight;
 import com.ms.flight.service.FlightService;
 
 import jakarta.validation.Valid;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -26,7 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("voos")
+@RequestMapping("/voos")
 public class FlightController {
 
     @Autowired
@@ -45,22 +41,19 @@ public class FlightController {
     }
 
     @GetMapping
-    public ResponseEntity<FlightByAirportDTO> searchFlightsByAirport(
+    public ResponseEntity<?> searchFlights(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data,
-            @RequestParam String origem,
-            @RequestParam String destino
+            @RequestParam(required = false) String origem,
+            @RequestParam(required = false) String destino,
+            @RequestParam(name = "data-fim", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim
     ) {
-         
-        return ResponseEntity.ok(flightService.searchFlightsByAirport(data, origem, destino));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<FlightWithAirportResponseDTO>> searchFlightsByDate(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data,
-            @RequestParam(name = "data-fim") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim
-    ) {
-        
-        return ResponseEntity.ok(flightService.searchFlightsByDate(data, dataFim));
+        if (origem != null && destino != null && dataFim == null) {
+            return ResponseEntity.ok(flightService.searchFlightsByAirport(data, origem, destino));
+        }
+        if (dataFim != null && origem == null && destino == null) {
+            return ResponseEntity.ok(flightService.searchFlightsByDate(data, dataFim));
+        }
+        return ResponseEntity.badRequest().body("Informe origem/destino ou intervalo de datas");
     }
     
 }
