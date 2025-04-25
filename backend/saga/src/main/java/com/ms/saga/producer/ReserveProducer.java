5db30dcdbe72a5e92d52bd873a28a26a93594dcb
Gabriel Speranceta <gabriel.hesperanceta@gmail.com>
2025-04-25 16:13:00 -0300

    feat: implement airport creation and flight status update functionality

--- backend/saga/src/main/java/com/ms/saga/producer/ReserveProducer.java
+++ backend/saga/src/main/java/com/ms/saga/producer/ReserveProducer.java
@@ -4,10 +4,22 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.ms.saga.config.RabbitMQConfig;
+import com.ms.saga.dto.flight.FlightStatusDTO;
+
 @Component
 public class ReserveProducer {
     
     @Autowired
     private RabbitTemplate rabbitTemplate;
+
+    //Isso sera para atualizar caso o voo tenha mudado estado
+    public void updateStatusReserve(FlightStatusDTO dto) {
+        rabbitTemplate.convertAndSend(
+            RabbitMQConfig.UPDATE_RESERVE_EXCHANGE,
+            RabbitMQConfig.UPDATE_RESERVE_ROUTING_KEY,
+            dto
+        );
+    }
 
 }
