66df81547099347659872f8d0ee15801519d5943
Gabriel Speranceta <gabriel.hesperanceta@gmail.com>
2025-04-26 12:47:53 -0300

    refactor: simplify service URL definitions using destructured environment variables

--- backend/api_gateway/config/services.js
+++ backend/api_gateway/config/services.js
@@ -1,11 +1,20 @@
+
 require('dotenv-safe').config();
 
+const {
+  MS_AUTH_PORT,
+  MS_CUSTOMER_PORT,
+  MS_EMPLOYEE_PORT,
+  MS_FLIGHT_PORT,
+  MS_RESERVE_PORT,
+  SAGA_PORT,
+} = process.env;
+
 module.exports = {
-    AUTH: "http://localhost:" + process.env.MS_AUTH_PORT || 'http://localhost:8081',
-    CUSTOMER: "http://localhost:" + process.env.MS_CUSTOMER_PORT || 'http://localhost:8082',
-    EMPLOYEE: "http://localhost:" + process.env.MS_EMPLOYEE_PORT || 'http://localhost:8083',
-    FLIGHT: "http://localhost:" + process.env.MS_FLIGHTS_PORT || 'http://localhost:8084',
-    RESERVATION: "http://localhost:" + process.env.MS_RESERVE_PORT || 'http://localhost:8085',
-    SAGAS: "http://localhost:" + process.env.SAGAS_PORT|| 'http://localhost:8087',
+  AUTH:        `http://localhost:${MS_AUTH_PORT || 8081}`,
+  CUSTOMER:    `http://localhost:${MS_CUSTOMER_PORT || 8082}`,
+  EMPLOYEE:    `http://localhost:${MS_EMPLOYEE_PORT || 8083}`,
+  FLIGHT:      `http://localhost:${MS_FLIGHT_PORT || 8084}`,
+  RESERVATION: `http://localhost:${MS_RESERVE_PORT || 8085}`,
+  SAGAS:       `http://localhost:${SAGA_PORT || 8087}`,
 };
-  
