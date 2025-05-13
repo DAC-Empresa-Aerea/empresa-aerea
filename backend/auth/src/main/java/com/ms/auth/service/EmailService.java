e44c344897e36857605309411480768b04a80a33
Luccachl <lucca.hmcps@gmail.com>
2025-05-13 17:17:16 -0300

    feat: EmailService. Falta criar um email para uso especifico + senha de aplicação

--- backend/auth/src/main/java/com/ms/auth/service/EmailService.java
+++ backend/auth/src/main/java/com/ms/auth/service/EmailService.java
@@ -1,12 +1,28 @@
 package com.ms.auth.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     public void sendPasswordEmail (String email, String password) {
+        try{
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Senha FlyHigh");
+        message.setText("Sua senha do FlyHigh é " + password + "!");
+        mailSender.send(message);
         System.out.println("Email enviado para " + email + " com a senha: " + password);
+        }catch (Exception e){
+            System.out.println("Erro ao enviar email: " + e.getMessage());
+        }
+
     }
 
 }
